a0d1d631f62bc872e9ffbec0a8e5c9133764a601
Jack Gray <jackgray@pop-os.localdomain>
2022-01-21 12:47:10 -0500

    debugging embedded null byte error

--- extract_nyspi_headers.py
+++ extract_nyspi_headers.py
@@ -1,46 +1,80 @@
-from os import mkdir, path, makedirs
+#!/home/sdc/anaconda3/bin/python36
+
+from subprocess import run
+from zlib import compress
+from datetime import date
+from os import mkdir, path, makedirs, rename
 from shutil import move
 from glob import glob
 import matlab.engine
 import io
+from copy import copy
 
 eng = matlab.engine.start_matlab()
 out = io.StringIO()
 err = io.StringIO()
 
-pfiles = glob('/home/sdc/data/rawdata/*.7')
+rawdata_path = '/home/sdc/data/rawdata'
+pfiles = glob(rawdata_path + '/*.7')
+compressed_pfiles = glob(rawdata_path + '/*.gz')
+converted_path = "/home/sdc/data/rawdata/converted"
+log_path = path.join(converted_path, "change_map.log")
+print("\n\nScaning rawdata for new P-files to convert...")
 
-for pfile in pfiles:
-    orig_filename = os.path.basename(pfile)
-    proj_id, subj_id, exam_no, scan_no, series_desc = eng.extract_nyspi_headers(pfile,stdout=out,stderr=err)
-    # Format MATLAB returns
-    subj_id = subj_id.replace(" ", "").replace("_", "").replace("-","")
-    series_desc = series_desc.replace(" ", "").replace("_", "-")
-    
-    print("\n\n\n*******************************************************************")
-    print("\nPulled the following header info from ", orig_filename)
-    print("Project ID: ", proj_id)
-    print("Subj ID: ", subj_id)
-    print("Exam #: ", exam_no)
-    print("Scan #: ", scan_no)
-    print("Series ID: ", series_desc)
-    
-    proj_path = path.join("/home/sdc/data/rawdata/converted", proj_id)
-    makedirs(proj_id, exist_ok=True)
-    
-    filename = 'sub-' + subj_id + '_ses-' + exam_no + '_series-' + scan_no + '_' + series_desc
-    
-    target_path = path.join(proj_path, filename)
-    
-    with open(change_map.log, 'a') as logfile:
-        logfile.write(orig_filename + "\t>>>\t" + filename)
-    
-    print(orig_filename + "\t>>>\t" + filename)
-    print(pfile + "\t>>>\t" + target_path)
-    
-    #move(pfile, target_path)
-    
-    print("*******************************************************************")
+if len(pfiles) > 0:
+	print("Detected new files in rawdata to be backed up")
+	date = date.today().strftime("%b-%d-%Y")
+	
+	with open(log_path, 'a') as logfile:
+		logfile.write("\n" + date + "\n")
+	
+	for pfile in pfiles:
+		# Call MATLAB to extract P-file header info
+		proj_id, subj_id, exam_no, scan_no, series_desc = eng.extract_nyspi_headers(pfile, nargout=5, stdout=out, stderr=err)
+		# Format MATLAB returns
+		proj_id = proj_id.replace(" ", "").replace("_", "").replace("-", "").lower()
+		subj_id = subj_id.replace(" ", "").replace("_", "").replace("-","").lower()
+		series_desc = series_desc.replace(" ", "").replace("_", "-").lower()
+		# Display results
+        orig_filename = path.basename(pfile)
+		print("\n\n\n*******************************************************************")
+		print("Pulled the following header info from ", orig_filename)
+		print("Project ID: ", proj_id)
+		print("Subj ID: ", subj_id)
+		print("Exam #: ", exam_no)
+		print("Scan #: ", scan_no)
+		print("Series ID: ", series_desc)
+		
+        # Case for blank subj_id default to 'test' project 
+		if len(proj_id) > 0:
+        		proj_path = path.join(converted_path, proj_id)
+		else:
+        		proj_path = path.join(converted_path, "test")
+		# Create dir structure
+		makedirs(proj_path, exist_ok=True)
+        
+        # New formated filename as compressed .gz 	
+		new_filename = 'sub-' + subj_id + '_ses-' + exam_no + '_series-' + scan_no + '_' + series_desc + ".7"
+		
+		target_path = str(path.join(proj_path, new_filename))		
 
-    
-    
+		print("Moving and compressing file...")
+#		print(orig_filename + " >>> " + filename)
+		print(pfile + " >>> " + target_path)
+		
+		original_data = open(pfile, 'rb').read()
+		compressed_data = compress(original_data, 1)
+		#zipfile = orig_filename.replace(".7", ".gz")    # Replace .7 extension with .gz
+		#print("zip dst: ", zip_dst)
+		# with open(target_path, 'wb') as zipfile:
+		# 	zipfile.write(compressed_data)
+        src = pfile
+        dst = target_path
+		run(['mv', src, dst])
+        with open(log_path, 'a') as logfile:
+        		logfile.write(orig_filename + "\t" + new_filename + "\n")
+		
+		
+		print("*******************************************************************")
+		
+			
